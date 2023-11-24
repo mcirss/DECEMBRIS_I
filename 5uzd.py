ae501724bc98e69663bccbb6d3faf1aab9076fd3
mcirss <markusscirss.mcs@gmail.com>
2023-11-24 14:33:20 +0200

    5uzd izpildits

--- 5uzd.py
+++ 5uzd.py
@@ -1,5 +1,5 @@
-skaitlis = int(input("Ievadiet skaitli: "))
-faktorials = 1
+skaitlis=int(input("Ievadiet skaitli: "))
+faktorials=1
 for i in range(1, skaitlis + 1):
-    faktorials *= i
+    faktorials*=i
 print(f"Faktoriāls no {skaitlis} ir: {faktorials}")
